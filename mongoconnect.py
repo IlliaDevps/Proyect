c538bf12a988c672f4a2f9e3d2237f2c1d3d4a4f
Illia Duverkher Natiahin/Smart TV Apps (SWQA) /SRPOL/Professional/Samsung Electronics <i.duverkher@samsung.com>
2023-06-21 16:31:59 +0200

    Added mongoconnect.py connection driver

--- mongoconnect.py
+++ mongoconnect.py
@@ -1,3 +1,4 @@
+import re
 import asyncio
 from pymongo import MongoClient
 
@@ -15,7 +16,7 @@ client = MongoClient(uri)
 # Function to connect to the server and gather data
 
 
-async def list_databases(client):
+async def appTestingList(client):
     databases_list = client.list_database_names()
     print("Databases found:")
     for db in databases_list:
@@ -28,39 +29,56 @@ async def list_databases(client):
     for collection_name in collection_names:
         print(f"- {collection_name}")
 
-    collection = dbase["app_history_reason"]
-    query = {
-        "aaData": {
-            "$elemMatch": {
-                "$or": [
-                    {
-                        "$and": [
-                            {
-                                "END_DT": {"$regex": "^9999"},
-                                "CERT_STEP_NM": "Verification"
-                            }
-                        ],
-                        "$and": [
-                            {
-                                "END_DT": {"$regex": "^9999"},
-                                "CERT_STEP_NM": "SP Review"
-                            }
-                        ]
-                    }
-                ]
+    collection_app_history_reason = dbase["app_history_reason"]
+    # collection_version_basic_info = dbase["version_basic_info"]
+    '''  queryTesting = {
+            "aaData": {
+                "$elemMatch": {
+                    "$and": [
+                        {
+                            "END_DT": {"$regex": "^9999"},
+                            "$or": [
+                                {"CERT_STEP_NM": "Verification"},
+                                {"CERT_STEP_NM": "SP Review"}
+                            ]
+                        }
+                    ]
+                }
             }
         }
-    }
+    '''
     count = 0
-    documents = collection.find(query)
-    for document in documents:
-        print(document["_key"])
-        count += 1
-    print(count)
+    documentsTesting = collection_app_history_reason.find()
+    # documents_version = collection_version_basic_info
+    
+    with open("keys.txt", "w") as file:
+        print("APP_ID__________ObjectID______________Version_________________Cert Step________MDL_STATUS")
+
+        for document in documentsTesting:
+            key = document["_key"]
+            Oid = document["_id"]
+            aaData = document.get("aaData", [])
+            appVersion = aaData[0].get("APP_VER_INFO")
+            for item in aaData:
+                if item.get("APP_VER_INFO") == appVersion:
+
+                    if (re.match(r"9999", item.get("END_DT")) and (item.get("MDL_STATUS_NM") == "Request Test" or item.get("MDL_STATUS_NM") == "Testing" or item.get("MDL_STATUS_NM") == "Tested (Pass)"
+                                                                   or item.get("MDL_STATUS_NM") == "Tested(Fail)")):
+                        # optional conditional re.match(r"9999", item.get("END_DT")) and (item.get("CERT_STEP_NM") == "Verification" or item.get("CERT_STEP_NM") == "SP Review")
+                        print(key, Oid, appVersion, item.get("END_DT"),
+                              item.get("CERT_STEP_NM"), item.get("MDL_STATUS_NM"),  sep=" ")
+                        file.write(
+                            f"AppID:{key}, ObjectID:{Oid}, AppVersion:{appVersion} EndDate: \n")
+                        count += 1
+                        break
+                else:
+                    break
+
+    print("Total apps count", count)
     client.close()
 
 
 async def main():
-    await list_databases(client)
+    await appTestingList(client)
 
 asyncio.run(main())
