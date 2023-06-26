ab172a68cee6742f4174084d0991a1924df66166
Illia Duverkher Natiahin/Smart TV Apps (SWQA) /SRPOL/Professional/Samsung Electronics <i.duverkher@samsung.com>
2023-06-26 14:30:21 +0200

    adding some new ideas

--- verificationTest.py
+++ verificationTest.py
@@ -29,53 +29,31 @@ async def appTestingList(client):
     for collection_name in collection_names:
         print(f"- {collection_name}")
 
-    collection_app_history_reason = dbase["verification_test"]
-    # collection_version_basic_info = dbase["version_basic_info"]
-    '''  queryTesting = {
-            "aaData": {
-                "$elemMatch": {
-                    "$and": [
-                        {
-                            "END_DT": {"$regex": "^9999"},
-                            "$or": [
-                                {"CERT_STEP_NM": "Verification"},
-                                {"CERT_STEP_NM": "SP Review"}
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-    '''
-    count = 0
-    documentsTesting = collection_app_history_reason.find()
-    # documents_version = collection_version_basic_info
+    collection_verification_test = dbase["verification_tests"]
+    query = {'TEST_STAT_NM': 'Testing'}
+    sort = [('REQ_DT', -1)]
 
+    count = 0
+    documentsTesting = collection_verification_test.find(query).sort(sort)
+    # documents_version = collection_version_basic_info
+    documentAppIDList = []
+    documentUniqueID = []
     with open("keys.txt", "w") as file:
-        print("APP_ID________Version_________________Cert Step____MDL_STATUS__________ObjectID____")
+        print("APP_ID________Version____TesterName")
 
         for document in documentsTesting:
-            key = document["_key"]
-            Oid = document["_id"]
-            aaData = document.get("aaData", [])
-            lastAppVersion = aaData[0].get("APP_VER_INFO")
-            for item in aaData:
-                if item.get("APP_VER_INFO") == lastAppVersion:
-                    currentDate = item.get("END_DT")
-                    modelStatus = item.get("MDL_STATUS_NM")
-                    certStep = item.get("CERT_STEP_NM")
 
-                    if re.match(r"9999", currentDate) and (modelStatus in ["Request Test", "Testing", "Tested(Pass)", "Tested(Fail)"]):
-                        # optional conditional re.match(r"9999", item.get("END_DT")) and (item.get("CERT_STEP_NM") == "Verification" or item.get("CERT_STEP_NM") == "SP Review")
-                        # optional conditional certStep in ["Verification", "SP Review"]
-                        print(key, lastAppVersion, currentDate,
-                              certStep, modelStatus, Oid, sep=" ")
-                        file.write(
-                            f"AppID:{key}, AppVersion: {lastAppVersion}, CertStep: {certStep} ModelStatus: {modelStatus},ObjectID: {Oid}  \n")
-                        count += 1
-                        break
-                else:
-                    break
+            appID = document["APP_ID"]
+            # print(appID)
+            appVersion = document["APP_VER_INFO"]
+            testername = document["TESTER_NM"]
+            if appID not in documentAppIDList:
+                documentAppIDList.append(document["APP_ID"])
+                documentUniqueID.append(document)
+                print(appID, appVersion, testername, sep=" ")
+                file.write(
+                    f"AppID  :{appID}, AppVersion  : {appVersion}, Testername  : {testername}  \n")
+                count += 1
 
     print("Total apps count", count)
     client.close()
